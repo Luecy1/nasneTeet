ca83253eab40a408d5366c58036b2463058e165e
luecy1 <kowaheno@gmail.com>
2017-05-04 10:03:38 +0900

    番組データ取得方法を１件ずつ読み込むように修正。

--- nasneTweet.rb
+++ nasneTweet.rb
@@ -5,6 +5,26 @@ require 'twitter'
 
 require 'time'
 
+
+def pickEffectData(data)
+  # dataの空チェック
+  if data['item'].empty? then
+    return nil
+  end
+
+  #最後を取得
+  lastData = data['item'].pop
+  
+  #有効かチェックし有効でない場合ばもう一度求める。
+  if  lastData['eventId'] == 65536
+    return pickEffectData(data)
+  end
+
+  #有効である場合、それを返す
+  return lastData
+end
+
+=begin
 twconfig = YAML.load(open('config.yaml'))
 client = Twitter::REST::Client.new do |config|
  config.consumer_key = twconfig['consumer_key']
@@ -12,64 +32,44 @@ client = Twitter::REST::Client.new do |config|
  config.access_token = twconfig['access_token']
  config.access_token_secret = twconfig['access_token_secret']
 end
-
+=end
 while true do
 
 begin
 
-url = 'http://192.168.0.11:64220/schedule/reservedListGet?searchCriteria=0&filter=0&startingIndex=0&requestedCount=0&sortCriteria=0&withDescriptionLong=0&withUserData=1'
-#url = 'reservedListGet.json' #LocalTest用
+#url = 'http://192.168.0.11:64220/schedule/reservedListGet?searchCriteria=0&filter=0&startingIndex=0&requestedCount=0&sortCriteria=0&withDescriptionLong=0&withUserData=1'
+url = 'reservedListGet.json' #LocalTest用
 data = JSON.load(open(url).read)
 
-#初期表示時、時間と逆なので反転
-data['item'].reverse!
+#最後を取得
+lastData = pickEffectData(data)
 
-today = Date.today
-todaydata = Array.new
-
-data['item'].each do |x|
-  date = Date.parse(x['startDateTime'])
-
-  #errorは除外
-  if  x['eventId'] == 65536
-    next
-  end
-
-  if date == today then
-    time = Time.parse(x['startDateTime'])
-
-    if Time.now < time then
-      todaydata << x
-    end
-  end
+#最後を取得できなかった場合24時までsleep
+if lastData == nil then
+  sleep Time.parse("24:00") - Time.now
+  next
 end
 
-todaydata.each do |x|
-  cleanTitle = x['title']
-    .gsub(/\ue192/,"【再】")
-    .gsub(/\ue195/,"【終】")
-    .gsub(/\ue0fe/,"【字】")
-    .gsub(/\ue180/,"【デ】")
-    .gsub(/\ue184/,"【解】")
-    .gsub(/\ue183/,"【多】")
-    .gsub(/\ue193/,"【新】")
-  time = Time.parse(x['startDateTime']) - Time.now
+cleanTitle = lastData['title']
+  .gsub(/\ue192/,"【再】")
+  .gsub(/\ue195/,"【終】")
+  .gsub(/\ue0fe/,"【字】")
+  .gsub(/\ue180/,"【デ】")
+  .gsub(/\ue184/,"【解】")
+  .gsub(/\ue183/,"【多】")
+  .gsub(/\ue193/,"【新】")
+time = Time.parse(lastData['startDateTime']) - Time.now
 
-  #sleepする
-  if time > 0 then
-    sleep time
-    client.update("【#{x['channelName']}】#{cleanTitle}　を録画中 #nasne")
-  elsif time > -60 then
-    client.update("【#{x['channelName']}】#{cleanTitle}　を録画中 #nasne")
-  end
-
-end#todaydata
+#sleepする
+if time > 0 then
+  sleep time
+  #client.update("【#{lastData['channelName']}】#{cleanTitle}　を録画中 #nasne")
+  puts "【#{lastData['channelName']}】#{cleanTitle}　を録画中 #nasne"
+end
 
 rescue => e
   puts e.message
   exit
 end
 
-# 日付変わるまで待機
-sleep Time.parse("24:00") - Time.now
 end#while
